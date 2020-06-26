523e4ad8373032bb3ffe9269c2e0dda3e735cdca
NoorShinwari <nanoshin746@gmail.com>
2020-06-26 10:26:35 +0200

    Decorators

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -4,21 +4,39 @@ function Logger(logString: string) {
     console.log(constructor);
   };
 }
-function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log("Rendering WithTemplete");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+// function WithTemplate(template: string, hookId: string) {
+//   return function (constructor: any) {
+//     console.log("Rendering WithTemplete");
+//     const hookEl = document.getElementById(hookId);
+//     const p = new constructor();
+//     if (hookEl) {
+//       hookEl.innerHTML = template;
+//       hookEl.querySelector("h1")!.textContent = p.name;
+//     }
+//   };
+// }
+
+function WithTemplate(templete: string, hookId: string) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = templete;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
+
 @Logger("Hi There Logger!")
-@WithTemplate("<h1>My Person Object</h1>", "app")
+@WithTemplate1("<h1>My Person Object</h1>", "app")
 class Person1 {
-  name = "Max";
+  name = "Noor";
 
   constructor() {
     console.log("Creating person object...");
@@ -76,3 +94,50 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+const pers = new Person1();
+console.log(pers);
+
+function WithTemplate1(template: string, hookId: string) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    constructor: T
+  ) {
+    return class extends constructor {
+      constructor(..._: any[]) {
+        super();
+
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+class Printer {
+  message = "This works";
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+const button2 = document.querySelector("button");
+button2!.addEventListener("click", p.showMessage);
